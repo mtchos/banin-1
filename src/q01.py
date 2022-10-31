ea38d36b4786813fee9d5a431c423cc921d3b629
mtchos <matheussilvaesousa@gmai.com>
2022-10-31 20:22:08 -0300

    First commit

---
+++ src/q01.py
@@ -0,0 +1,10 @@
+print("Brenda Pan Teixeira")
+print("Matheus Silva e Sousa")
+print("Questao 1")
+
+segundos = int(input())
+horas = int(segundos / 3600)
+segundos -= (horas * 3600)
+minutos = int(segundos / 60)
+segundos -= (minutos * 60)
+print("{} horas, {} minutos, {} segundos".format(horas, minutos, segundos))
---
+++ src/q02.py
@@ -0,0 +1,41 @@
+print("Brenda Pan Teixeira")
+print("Matheus Silva e Sousa")
+print("Questao 2")
+
+LMin = int(input("Digite um valor maior que 0: "))
+while LMin <= 0:
+    LMin = int(input("LMin deve ser um valor maior que 0: "))
+
+LMax = int(input(f"Digite um valor maior que LMin ({LMin}): "))
+while LMax <= LMin:
+    LMax = int(input(f"LMax deve ser um valor maior que LMin ({LMin}): "))
+
+X = int(input("Digite um valor diferente de zero: "))
+while X == 0:
+    X = int(input("X deve ser diferente de zero: "))
+
+lista = []
+valor_digitado = 1
+soma = 0
+
+while valor_digitado != 0:
+    valor_digitado = int(input())
+    esta_na_faixa = valor_digitado >= LMin and valor_digitado <= LMax
+    divisivel_por_x = valor_digitado % X == 0
+    esta_na_lista = False
+    for valor in lista:
+        if valor == valor_digitado:
+            esta_na_lista = True
+
+    if esta_na_faixa and divisivel_por_x and not esta_na_lista:
+        soma += valor_digitado
+        lista.append(valor_digitado)
+
+media = 0
+if len(lista) != 0:
+    media = soma / len(lista)
+
+print(lista)
+print(len(lista))
+print(soma)
+print(media)
